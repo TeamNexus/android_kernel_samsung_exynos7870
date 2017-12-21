d33fe3ed6436f8c516f50bb5c9ba5a535595a306
j0sh1x <aljoshua.hell@gmail.com>
2017-12-21 16:33:52 +0100

    upload upstreamed kernel by valera1978 and update binder for 64bit

---
+++ firmware/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_firmware/built-in.o :=  /home/j0sh1x/aarch64-linux-android-4.9/bin/aarch64-linux-android-ld -EL    -r -o firmware/built-in.o firmware/gnss_firmware.bin.gen.o firmware/tsp_stm/matisse.fw.gen.o firmware/tsp_stm/stm_gtaxl.fw.gen.o firmware/tsp_stm/fts1a096_gtaxl_rev03.fw.gen.o firmware/tsp_stm/fts1a096_gtaxl_note.fw.gen.o 
---
+++ firmware/.gnss_firmware.bin.gen.o.cmd
@@ -0,0 +1,9 @@
+cmd_firmware/gnss_firmware.bin.gen.o := /home/j0sh1x/aarch64-linux-android-4.9/bin/aarch64-linux-android-gcc -Wp,-MD,firmware/.gnss_firmware.bin.gen.o.d  -nostdinc -isystem /home/j0sh1x/aarch64-linux-android-4.9/bin/../lib/gcc/aarch64-linux-android/4.9.x/include -I./arch/arm64/include -Iarch/arm64/include/generated  -Iinclude -I./arch/arm64/include/uapi -Iarch/arm64/include/generated/uapi -I./include/uapi -Iinclude/generated/uapi -include ./include/linux/kconfig.h -D__KERNEL__ -mlittle-endian  -D__ASSEMBLY__ -Wa,-gdwarf-2         -c -o firmware/gnss_firmware.bin.gen.o firmware/gnss_firmware.bin.gen.S
+
+source_firmware/gnss_firmware.bin.gen.o := firmware/gnss_firmware.bin.gen.S
+
+deps_firmware/gnss_firmware.bin.gen.o := \
+
+firmware/gnss_firmware.bin.gen.o: $(deps_firmware/gnss_firmware.bin.gen.o)
+
+$(deps_firmware/gnss_firmware.bin.gen.o):
